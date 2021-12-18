8d2e8fdb2012a16d19cd6cb7820538368168ae83
Azdra <baptiste.brand2002@gmail.com>
2021-12-18 15:21:05 +0100

    Update emote commade base

--- src/commands/emote/emote.ts
+++ src/commands/emote/emote.ts
@@ -1,11 +1,8 @@
-import {
-  BaseCommand,
-  BaseCommandType,
-  CategoryInterface
-} from '../../components/baseCommand';
+import { BaseCommand, CategoryInterface } from '../../components/baseCommand';
 import { Message } from 'discord.js';
+import { commands } from '../../lib/constants';
 
-export default class Emote extends BaseCommand implements BaseCommandType {
+export default class Emote extends BaseCommand {
   alias: string[];
   allowDM: boolean;
   category: CategoryInterface;
@@ -14,8 +11,23 @@ export default class Emote extends BaseCommand implements BaseCommandType {
   disable: boolean;
   example: string;
   onlyDev: boolean;
+  multipleCommand: boolean;
 
-  execute(): Promise<Message> {
-    return Promise.resolve(undefined);
+  constructor(props) {
+    super(props);
+
+    this.alias = commands.get('emote');
+    this.allowDM = false;
+    this.category = 'emote';
+    this.cooldown = 2;
+    this.description = 'Need description';
+    this.disable = false;
+    this.example = `{prefix} [${commands.get('emote').join('|')}]`;
+    this.onlyDev = false;
+    this.multipleCommand = true;
+  }
+
+  execute(): Promise<Message<boolean>> {
+    throw new Error('Method not implemented.');
   }
 }
