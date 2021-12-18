10297403a7e6b3375281acc877abdbdbeb66601d
Azdra <baptiste.brand2002@gmail.com>
2021-12-18 15:21:22 +0100

    Add new constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,8 @@
-export const commands = new Map<string, string[]>();
+export type CommandCategory = 'emote' | 'reaction' | 'hentai' | 'porn';
+export const commands = new Map<CommandCategory, string[]>();
+export const commandsList = new Map<string, any>();
 export const getProjectDir = process.cwd();
-export const getAppDir = process.cwd() + '/src';
+export const getAppDir = getProjectDir + '/src';
 export const commandsDir = getAppDir + '/commands';
 export const eventsDir = getAppDir + '/events';
+export const socketDir = getAppDir + '/sockets';
