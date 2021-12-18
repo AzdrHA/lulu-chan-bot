53f38eecfbcaf7042420cc11c779b1a618a10535
Azdra <baptiste.brand2002@gmail.com>
2021-12-18 15:22:07 +0100

    add new config

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,11 +1,17 @@
+import UtilsStr from '../utils/utilsStr';
+
 type AppConfig = {
   api_domain: string;
   token: string;
   development: boolean;
+  owners: string[];
+  prefixes: string[];
 };
 
 export const AppConfig: AppConfig = {
   api_domain: process.env.API_URL,
   token: process.env.TOKEN,
-  development: Boolean(process.env.DEVELOPMENT) ?? true
+  development: Boolean(process.env.DEVELOPMENT) ?? true,
+  owners: UtilsStr.convertStringArrayToArray(process.env.OWNERS),
+  prefixes: UtilsStr.convertStringArrayToArray(process.env.PREFIXES)
 };
