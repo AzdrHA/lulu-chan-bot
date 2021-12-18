f2b5172319f619432caa0225e0e303061ca01b4d
Azdra <baptiste.brand2002@gmail.com>
2021-12-18 15:21:48 +0100

    Improve component application

--- src/components/application/application.ts
+++ src/components/application/application.ts
@@ -2,9 +2,27 @@ import { Client, ClientOptions } from 'discord.js';
 import { makeRequest } from '../../api/makeRequest';
 import { ApiConfig } from '../../config/apiConfig';
 import { Category } from '../../types/Category';
-import { commands, commandsDir, eventsDir } from '../../lib/constants';
+import {
+  CommandCategory,
+  commands,
+  commandsDir,
+  eventsDir,
+  socketDir
+} from '../../lib/constants';
 import print from '../../lib/print';
 import loadFiles from '../../lib/loadFiles';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
+
+// Create Socket Server
+const httpServer = createServer();
+export const io = new Server(httpServer, {
+  cors: {
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['my-custom-header'],
+    credentials: true
+  }
+});
 
 interface ApplicationOption extends ClientOptions {
   development: boolean;
@@ -29,11 +47,17 @@ class Application extends Client {
           category.commands.length
         );
         category.commands.forEach((command) => {
-          const hasCommand = commands.get(category.name) ?? [];
-          commands.set(category.name, hasCommand.concat([command.name]));
+          const hasCommand = commands.get(<CommandCategory>category.name) ?? [];
+          commands.set(
+            <CommandCategory>category.name,
+            hasCommand.concat([command.name])
+          );
         });
       });
-      return this.loadEvent();
+      return httpServer.listen(3232, () => {
+        print.info('Socket server is start');
+        return this.loadSocket();
+      });
     });
   }
 
@@ -41,16 +65,25 @@ class Application extends Client {
     return makeRequest(ApiConfig.get_all_commands, 'GET');
   }
 
+  private async loadSocket() {
+    print.info('Load Socket Event ...');
+    io.on('connection', (socket: Socket) => {
+      return loadFiles(socketDir, 'socket', this, io);
+    });
+
+    return this.loadEvent();
+  }
+
   private async loadEvent() {
     print.info('Load Event ...');
-    return loadFiles(eventsDir, 'event', this).then(() => {
+    return loadFiles(eventsDir, 'event', this, io).then(() => {
       return this.loadCommand();
     });
   }
 
   private async loadCommand() {
     print.info('Load Command ...');
-    return loadFiles(commandsDir, 'command', this).then(() => {
+    return loadFiles(commandsDir, 'command', this, io).then(() => {
       return this.login(this.token);
     });
   }
