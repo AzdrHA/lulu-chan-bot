ff763c4f65d1bbaec9fd9161f60dfd5f3245db34
Azdra <baptiste.brand2002@gmail.com>
2021-12-18 15:20:53 +0100

    Create base messageCreate event

--- src/events/message/messageCreate.ts
+++ src/events/message/messageCreate.ts
@@ -1,5 +1,18 @@
 import Application from '../../components/application/application';
+import { Message } from 'discord.js';
+import { AppConfig } from '../../config/appConfig';
 
-export default (client: Application) => {
+const messageCreate = (client: Application, message: Message) => {
+  if (message.author.bot) return;
+  if (client.development) {
+    if (
+      (AppConfig.owners && !AppConfig.owners.includes(message.author.id)) ||
+      (message.channel.id !== '653610950141935668' &&
+        message.channel.id !== '685214662572507138')
+    )
+      return;
+  }
+
   console.log("Hello i'm messageCreate event");
 };
+export default messageCreate;
