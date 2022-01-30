905ba69e7bb71ad121a6c1f8ed1179ec244b6eed
Azdra <baptiste.brand2002@gmail.com>
2022-01-30 23:34:46 +0100

    Add new intents

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,15 @@ import { Intents } from 'discord.js';
 import { AppConfig } from './config/appConfig';
 
 new Application({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  partials: ['CHANNEL'],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Intents.FLAGS.GUILD_MEMBERS,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+  ],
   development: AppConfig.development,
   token: AppConfig.token
 });
