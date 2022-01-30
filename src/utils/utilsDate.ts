ee046346ccc9d1414859de80cc28b0378ecd8de4
Azdra <baptiste.brand2002@gmail.com>
2022-01-30 23:31:56 +0100

    Create class UtilsDate

---
+++ src/utils/utilsDate.ts
@@ -0,0 +1,15 @@
+export default class UtilsDate {
+  public static formatTime = (ms: number): string => {
+    const timeMs = new Date(ms);
+    const minutes = timeMs.getMinutes();
+    const seconds = timeMs.getSeconds();
+    const milliseconds = timeMs.getMilliseconds();
+
+    let time = '';
+    time += minutes ? '' + minutes : '';
+    time += seconds ? '' + seconds : seconds;
+    time += '.' + milliseconds;
+
+    return time;
+  };
+}
