dbe9ebeb3f2a06927d79e3e1fabb1dbc612a1c46
Azdra <baptiste.brand2002@gmail.com>
2022-01-30 23:35:46 +0100

    changes

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -1,11 +1,15 @@
 type ApiConfig = {
   get_all_commands: string;
-  get_or_create_setting(guildId: string): string;
+  get_or_create_or_update_setting(guildId: string): string;
+  get_image_by_command(command: string): string;
 };
 
 export const ApiConfig: ApiConfig = {
   get_all_commands: 'command/category/commands',
-  get_or_create_setting(guildId: string) {
+  get_or_create_or_update_setting(guildId: string) {
     return `guild/setting/${guildId}`;
+  },
+  get_image_by_command(command: string) {
+    return `image/${command}`;
   }
 };
