c9e3aae7501ddee42bd14d043a640c9b014555d6
Azdra <baptiste.brand2002@gmail.com>
2022-02-01 17:34:02 +0100

    Add client presence

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -8,6 +8,16 @@ const ready = async (client: Application) => {
   await UtilsDiscord.updateGuildsStatus(client);
   await UtilsDiscord.updateMembersStatus(client);
 
+  client.user.setPresence({
+    status: 'idle',
+    activities: [
+      {
+        name: 'Heir to the throne of the peasantry',
+        type: 'PLAYING'
+      }
+    ]
+  });
+
   console.log(`Logged in as ${client.user.tag}!`);
 };
 export default ready;
