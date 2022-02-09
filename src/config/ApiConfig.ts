dcc4139b341831ff266f6fcc4b355d905dbeb606
Azdra <baptiste.brand2002@gmail.com>
2022-02-09 13:40:19 +0100

    Reorganization code

--- src/config/ApiConfig.ts
+++ src/config/ApiConfig.ts
@@ -6,9 +6,19 @@ type ApiConfig = {
 
 export const ApiConfig: ApiConfig = {
   get_all_commands: 'command/category/commands',
+
+  /**
+   * @param {string} guildId
+   * @return {string}
+   */
   get_or_create_or_update_setting(guildId: string) {
     return `guild/setting/${guildId}`;
   },
+
+  /**
+   * @param {string} command
+   * @return {string}
+   */
   get_image_by_command(command: string) {
     return `image/${command}`;
   }
