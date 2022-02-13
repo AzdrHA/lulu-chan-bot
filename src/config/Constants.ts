39a2123715154a1ce2c135eebfbb57c292c38d8f
Azdra <baptiste.brand2002@gmail.com>
2022-02-13 20:53:59 +0100

    Fix constant fix + add new

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,8 +1,10 @@
 import { Category } from '../types/Category';
+import { Blacklist } from '../types/Blacklist';
 
 export const commands = new Map<Category, string[]>();
+export const owners = new Set();
 export const commandsList = new Map<string, any>();
-export const blacklists = new Map<string, any>();
+export const blacklists = new Map<string, Blacklist>();
 export const getProjectDir = process.cwd();
 export const getAppDir = getProjectDir + '/src';
 export const commandsDir = getAppDir + '/commands';
